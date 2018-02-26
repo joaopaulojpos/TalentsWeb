7bd6477a8f25d9ef59350228a20586879ee06374
MaikonSilva <maikon.silva@hotmail.com.br>
2018-02-26 20:52:13 -0300

    Ajustes API Profissional
    Ajustes API Profissional

--- api/src/model/dados/DAOProfissional.php
+++ api/src/model/dados/DAOProfissional.php
@@ -9,24 +9,35 @@ class DaoProfissional implements iDAOProfissional
 		
 	}
 	public function cadastrar(Profissional $u){
-		$comando = "insert into profissional (nome, sobrenome, login, senha) values (:nome, :sobrenome, :login, :senha)";
+		$comando = "insert into profissional (b_foto,ds_senha,dt_nascimento,ds_email,nr_latitude,nr_longitude,tp_conta,tp_sexo,ds_nome) 
+		            values (:b_foto,:ds_senha,:dt_nascimento,:ds_email,:nr_latitude,:nr_longitude,:tp_conta,:tp_sexo,:ds_nome)";
 		$stmt = db::getInstance()->prepare($comando);
 		$run = $stmt->execute(array(
-    			':nome' => $u->getNome(),
-    			':sobrenome' => $u->getSobrenome(),
-    			':login' => $u->getLogin(),
-				':senha' => $u->getSenha()
+    			':b_foto' => $u->getBfoto(),
+    			':ds_senha' => $u->getDsSenha(),
+    			':dt_nascimento' => $u->getDtNascimento(),
+				':ds_email' => $u->getDsEmail(),
+				':nr_latitude' => $u->getNrlatitude(),
+				':nr_longitude' => $u->getNrlogitude(),
+				':tp_conta' => $u->getTpconta(),
+				':tp_sexo' => $u->getTpsexo(),
+				':ds_nome' => $u->getDsnome(),
  		));
 	}
 	public function alterar(Profissional $u){
-		$comando = "update tb_Empresa set nome = :nome, sobrenome = :sobrenome, login = :login, senha = :senha where id = :id";
+		$comando = "update profissional set b_foto = :b_foto, ds_senha = :ds_senha, dt_nascimento = :dt_nascimento, ds_email = :ds_email,nr_latitude= :nr_latitude,nr_longitude = :nr_latitude,tp_conta= :tp_conta,tp_sexo = :tp_sexo,ds_nome = :ds_nome where cd_profissional = :cd_profissional";
 		$stmt = db::getInstance()->prepare($comando);
 		$run = $stmt->execute(array(
-    			':nome' => $u->getNome(),
-    			':sobrenome' => $u->getSobrenome(),
-    			':login' => $u->getLogin(),
-				':senha' => $u->getSenha(),
-				':id' => $u->getId()
+				':b_foto' => $u->getBfoto(),
+    			':ds_senha' => $u->getDsSenha(),
+    			':dt_nascimento' => $u->getDtNascimento(),
+				':ds_email' => $u->getDsEmail(),
+				':nr_latitude' => $u->getNrlatitude(),
+				':nr_longitude' => $u->getNrlogitude(),
+				':tp_conta' => $u->getTpconta(),
+				':tp_sexo' => $u->getTpsexo(),
+				':ds_nome' => $u->getDsnome(),
+				':cd_profissional' => $u->getCdProfissional()
  		));
 	}
 	public function excluir(Profissional $u){
