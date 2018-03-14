603474769b403c248fa6b56c6f4683bb3a477e39
Rhuan Dantas <rhuandantas10@gmail.com>
2018-03-14 08:31:58 -0300

    Algumas alterações na consulta da vaga. Ainda falta refinar e ajustar o banco para atender melhor a funcionalidade.

--- api/README.md
+++ api/README.md
@@ -24,4 +24,5 @@ $ composer
 $ http://localhost/talentsweb/api/public/api/profissional/login
 $ http://localhost/talentsweb/api/public/api/empresa/login
 $ http://localhost/talentsweb/api/public/api/vaga/publicar
+$ http://localhost/talentsweb/api/public/api/vaga/curtirVaga
 ```
--- api/src/model/dados/DAOVaga.php
+++ api/src/model/dados/DAOVaga.php
@@ -29,14 +29,19 @@ class DaoVaga implements iDAOVaga
     }
 
     public function pesquisar(Vaga $vaga, $alt='false'){
-        $comando = 'select * from vaga ';
+        $comando = 'select v.cd_vaga,nr_qtd_vaga,ds_observacao,dt_validade,tp_contratacao,nr_longitude,nr_latitude,ds_beneficios,ds_horario_expediente,dt_criacao,ds_titulo,vl_salario,cargo.cd_cargo,ds_cargo,e.cd_empresa,
+ ds_razao_social,ds_nome_fantasia,nr_porte,ds_nome_responsavel,ds_area_atuacao,ds_site,ds_telefone,nr_cnpj,ds_email,ds_senha,f.cd_formacao,ds_formacao,vh.cd_habilidade,vh.nr_nivel,h.ds_habilidade,vi.cd_idioma,ds_idioma
+ from vaga AS v JOIN vaga_curso AS vcurso ON v.cd_vaga = vcurso.cd_vaga
+JOIN vaga_habilidade as vh ON vh.cd_vaga = v.cd_vaga JOIN vaga_idioma as vi ON vi.cd_vaga = v.cd_vaga JOIN cargo ON cargo.cd_cargo = v.cd_cargo
+ JOIN empresa as e ON e.cd_empresa = v.cd_empresa JOIN formacao AS f ON f.cd_formacao = vcurso.cd_formacao JOIN habilidade AS h ON h.cd_habilidade = vh.cd_habilidade
+ JOIN idioma AS i ON i.cd_idioma = vi.cd_idioma;';
         $where = '';
 
         if (!empty($vaga->getCdVaga())){
             if (empty($where)){
-                $where = ' where cd_vaga = :cd_vaga';
+                $where = ' where v.cd_vaga = :cd_vaga';
             }else{
-                $where = $where . ' and cd_vaga = :cd_vaga';
+                $where = $where . ' and v.cd_vaga = :cd_vaga';
             }
         }
 
