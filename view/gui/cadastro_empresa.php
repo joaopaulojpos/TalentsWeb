552431c47a14d2f6b90811ec187e9a568e20197c
joaopaulojpos <jpo.oliveiras@gmail.com>
2018-03-31 01:47:32 -0300

    Cadastro de Empresa com Problemas no Cadastrar e com Problemas na mascara do Contato

--- view/gui/cadastro_empresa.php
+++ view/gui/cadastro_empresa.php
@@ -1,3 +1,38 @@
+<?php
+
+if (isset($_SESSION['empresa'])) 
+session_destroy();
+
+$cd_empresa              = '';
+$nr_cnpj                 = '';
+$ds_razao_social         = '';
+$ds_nome_fantasia        = '';
+$nr_porte                = '';
+$ds_area_atuacao         = '';
+$ds_responsavel_cadastro = '';
+$ds_telefone             = '';
+$ds_site                 = '';
+$ds_email                = '';
+$ds_senha                = '';
+
+if (isset($_SESSION['empresaLogada'])) {   //Verifica se há seções
+  $empresa = $_SESSION['empresaLogada']; 
+
+  $cd_empresa = $empresa[0]['cd_empresa'];
+  $nr_cnpj = $empresa[0]['nr_cnpj'];
+  $ds_razao_social = $empresa[0]['ds_razao_social'];
+  $ds_nome_fantasia = $empresa[0]['ds_nome_fantasia'];
+  $nr_porte = $empresa[0]['nr_porte'];
+  $ds_area_atuacao = $empresa[0]['ds_area_atuacao'];
+  $ds_responsavel_cadastro = $empresa[0]['ds_nome_responsavel'];
+  $ds_telefone = $empresa[0]['ds_telefone'];
+  $ds_site = $empresa[0]['ds_site'];
+  $ds_email = $empresa[0]['ds_email'];
+  $ds_senha = $empresa[0]['ds_senha'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -31,76 +66,88 @@
                 <div class="row">
                     <div class="input-field col s12 m10">
                         <i class="material-icons prefix">info</i>
-                        <input name="cnpj" id="cnpj" type="text" class="validate" minlength="3" required>
+                        <input name="cnpj" id="cnpj" type="text" value="<?php echo $nr_cnpj ?>" title="Digite o CNPJ no formato nn.nnn.nnn/nnnn-nn" placeholder="00.000.000/0000-00" class="validate">
                         <label for="titulo">CNPJ</label>
                     </div>
 
                     <div class="input-field col s12 m2">
-                        <a class="waves-effect waves-light btn btn-small teal darken-1">Buscar</a>
+                        <button class="btn waves-effect waves-light btn-small teal darken-1" type="submit" name="buttonCarregarDados" id="buttonCarregarDados" onclick="carregarDadosViaWebService()">Buscar
+                        </button>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">business_center</i>
-                        <input name="razaosocial" id="razaosocial" type="text" required>
+                        <input name="razaosocial" id="razaosocial" type="text" value="<?php echo $ds_razao_social ?>">
                         <label for="razaosocial">Razão Social</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">business_center</i>
-                        <input name="nomefantasia" id="nomefantasia" type="text" required>
+                        <input name="nomefantasia" id="nomefantasia" type="text" value="<?php echo $ds_nome_fantasia ?>">
                         <label for="nomefantasia">Nome Fantasia</label>
                     </div>
 
                     <div class="input-field col s12 m6">
                         <i class="material-icons prefix">book</i>
-                        <input id="areaatuacao" name="areaatuacao" type="text" class="validate" required>
+                        <input id="areaatuacao" name="areaatuacao" type="text" value="<?php echo $ds_area_atuacao ?>" class="validate">
                         <label for="titulo">Área de atuação</label>
                     </div>
 
                     <div class="input-field col s12 m6">
                         <select required>
                             <option value="" disabled selected>Selecione o porte atual</option>
-                            <option value="1" <?php echo $nr_porte=='1'?'selected':'';?></option>> 1 - 5</option>
-                            <option value="2">Option 2</option>
-                            <option value="3">Option 3</option>
+                            <option value="1" <?php echo $nr_porte=='1'?'selected':'';?>> 1 - 5 </option>
+                            <option value="2" <?php echo $nr_porte=='2'?'selected':'';?>> 6 - 10 </option>
+                            <option value="3" <?php echo $nr_porte=='3'?'selected':'';?>> 11 - 50 </option>
+                            <option value="4" <?php echo $nr_porte=='4'?'selected':'';?>> 51 - 100 </option>
+                            <option value="5" <?php echo $nr_porte=='5'?'selected':'';?>> 101 - 500 </option>
+                            <option value="6" <?php echo $nr_porte=='6'?'selected':'';?>> 500 + </option>
                         </select>
                         <label>Porte</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">assignment_ind</i>
-                        <input id="responsavel" name="responsavel" type="text" class="validate" required>
+                        <input id="responsavel" name="responsavel" type="text" value="<?php echo $ds_responsavel_cadastro ?>" class="validate">
                         <label for="titulo">Responsável pelo cadastro</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">contacts</i>
-                        <input id="telefone" name="telefone" type="tel" class="validate" required>
+                        <input id="telefone" name="telefone" type="text" value="<?php echo $ds_telefone ?>" placeholder="99 999999999" class="validate">
                         <label for="titulo">Contato</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">email</i>
-                        <input id="email" name="email" type="email" class="validate" required>
+                        <input id="email" name="email" type="text" value="<?php echo $ds_email ?>" class="validate">
                         <label for="titulo">Email</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">language</i>
-                        <input id="site" name="site" type="text" class="validate" required>
+                        <input id="site" name="site" type="text" value="<?php echo $ds_site ?>" class="validate">
                         <label for="titulo">Site</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">https</i>
-                        <input id="senha" name="senha" type="password" class="validate" required>
+                        <input id="senha" name="senha" type="password" value="<?php echo $ds_senha ?>" class="validate">
                         <label for="titulo">Senha</label>
                     </div>
 
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">https</i>
-                        <input id="senhaconfirmacao" name="senhaconfirmacao" type="password" class="validate" required>
+                        <input id="senhaconfirmacao" name="senhaconfirmacao" type="password" value="<?php echo $ds_senha ?>" class="validate">
                         <label for="titulo">Confirmar Senha</label>
+                    </div>
+
+                    <div class="input-field col s12 m12">
+                        <span id="loader"></span>
+                    </div>
+
+                    <div class="input-field col s12 m12">
+                        <span id="errMessage"></span>
                     </div>
                 </div>
             </div>
@@ -126,5 +173,128 @@
                 $('select').material_select();
             });
         </script>
+
+        <!--  Scripts-->
+  <script src="js/vaga.js"></script>
+
+  <script type='text/javascript'>
+    $(document).ready(function(){
+      $('#errMessage').hide(); //Esconde o elemento com id errolog
+      $('#loader').hide();
+
+
+      $('#telefone').mask('(00) 00000-0000');
+      $('#cnpj').mask('00.000.000/0000-00');
+
+      $('#email').mask("A", {
+        translation: {
+          "A": { pattern: /[\w@\-.+]/, recursive: true }
+        }
+      });
+
+      $('#site').mask("A", {
+        translation: {
+          "A": { pattern: /[\w@\-.+]/, recursive: true }
+        }
+      });
+
+      $('#formulario').submit(function(){  //Ao submeter formulário
+        $('#loader').show();
+        var codigo_empresa=$('#cd_empresa').val();
+        var cnpj=$('#cnpj').val();
+        var razao_social=$('#razaosocial').val();
+        var nome_fantasia=$('#nomefantasia').val();
+        var porte=$('#porte').val();
+        var area_atuacao=$('#areaatuacao').val();
+        var responsavel=$('#responsavel').val();
+        var telefone=$('#telefone').val();
+        var site=$('#site').val();
+        var email=$('#email').val();
+        var senha=$('#senha').val();
+        var senhaconfirmacao=$('#senhaconfirmacao').val();
+
+        if (senha != senhaconfirmacao){
+          document.getElementById('errMessage').innerHTML = 'A senha e a confirmação de senhas estão divergentes!';
+          $('#loader').hide();
+          $('#errMessage').show(); 
+          return false; 
+        }
+
+        $.ajax({      //Função AJAX
+          url:"valida_cadastro.php",      //Arquivo php
+          type:"post",        //Método de envio
+          data: "cd_empresa="+codigo_empresa+"&cnpj="+cnpj+"&razaosocial="+razao_social+"&nomefantasia="+nome_fantasia+"&porte="+porte+"&areaatuacao="+area_atuacao+"&responsavel="+responsavel+"&telefone="+telefone+"&site="+site+"&email="+email+"&senha="+senha, //Dados
+            success: function (result){     //Sucesso no AJAX
+                        if(result==1){            
+                          location.href='vaga.php';  //Redireciona
+                        }else{
+                          document.getElementById('errMessage').innerHTML = result;
+                          $('#loader').hide();
+                          $('#errMessage').show();   //Informa o erro
+                        }
+                    }
+        })
+        return false; //Evita que a página seja atualizada
+      })
+    });
+
+    function carregarDadosViaWebService() {
+      document.getElementById('loader').style.display = 'block';
+      var teste = document.getElementById('cnpj').value.replace(/[^\d]+/g,'');
+      $.ajax({      //Função AJAX
+        url:"carrega_dados_cadastro_empresa.php",      //Arquivo php
+        type:"post",        //Método de envio
+        data: "cnpj="+teste, //Dados
+          success: function (result){     //Sucesso no AJAX
+                      //alert(JSON.parse(result));
+                      //alert(JSON.parse(result).atividade_principal);
+                      /*for (var i = 0; i < ; i++){
+                          var value = object[i];
+                          var index = i;   // desnecessário, mas coloquei para clarificar
+                          alert(value);
+                      }*/ 
+                      try {
+                        var dados = JSON.parse(result);
+
+                        for (key in dados) {  
+                          if (key == 'status'){
+                            if (dados[key] == 'ERROR'){
+                              //document.getElementById('errMessage').innerHTML = 'Não foi possível carregar os dados do CNPJ digitado!';
+                              Materialize.toast('Não foi possível carregar os dados do cnpj digitado!', 4000)
+                              $('#loader').hide();
+                              $('#errMessage').show();   //Informa o erro 
+                              return false;
+                            }
+                          }else if (key == 'nome'){
+                            document.getElementById('razaosocial').value = dados[key];
+                          }else if (key == 'telefone'){
+                            document.getElementById('telefone').value = dados[key];
+                          }else if (key == 'fantasia'){
+                            document.getElementById('nomefantasia').value = dados[key];
+                          }else if (key == 'email'){
+                            document.getElementById('email').value = dados[key];
+                          }else if (key == 'atividade_principal'){ 
+                            for (atividade in dados[key][0]){
+                              if (atividade == 'text'){
+                                document.getElementById('areaatuacao').value = dados[key][0][atividade]; 
+                              }
+                            }
+
+                          }
+                        }
+                      }catch (e) {
+                        //document.getElementById('errMessage').innerHTML = 'Não foi possível carregar os dados do CNPJ digitado!';
+                        Materialize.toast('Não foi possível carregar os dados do cnpj digitado!', 4000)
+                        $('#loader').hide();
+                        $('#errMessage').show();   //Informa o erro
+                      }finally {
+                        $('#loader').hide();
+                      }
+                   }
+      });
+      //document.getElementById('loader').style.display = 'none';
+    }
+
+  </script>
 </body>
 </html>
