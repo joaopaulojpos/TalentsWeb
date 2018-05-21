f30ab46c58fc9b67ee868c6fb68d4d0418f2c154
joaopaulojpos <jpo.oliveiras@gmail.com>
2018-05-21 13:02:04 -0300

    Criando um novo layout para listagem de candidatos

---
+++ view/gui/candidato.php
@@ -0,0 +1,27 @@
+<?php
+include "menu.php";
+include "foooter.php";
+?>
+
+<section class="section">
+    <div class="row">
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                    <img src="https://lorempixel.com/100/190/nature/6">
+                </div>
+                <div class="card-stacked">
+                    <div class="card-content">
+                    <h5>João Paulo</h5>
+                    <h6> <i class="material-icons">school</i> Análise e Desenvolvimento de Sistemas </h6>
+                    <h6> <i class="material-icons">near_me</i> 8.06 km </h6>
+                    <h6> <i class="material-icons">star</i> Impulsivo,prático </h6>
+                    </div>
+                    <div class="card-action">
+                    <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
