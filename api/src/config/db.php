59cb74f680628287bdf4f36099bca9f2d422f338
Bruno Félix de Mendonça Barbosa <brunofelixbarbosa123@hotmail.com>
2018-06-08 00:52:51 -0300

    ajuste link db

--- api/src/config/db.php
+++ api/src/config/db.php
@@ -10,6 +10,7 @@
         if (!isset(self::$instance)) {
             //self::$instance = new PDO('mysql:host=localhost;dbname=id4854326_db_talents', 'id4854326_teste', '123456', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance = new PDO('mysql:host=localhost;dbname=id4854326_db_talents', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            //self::$instance = new PDO('mysql:host=mysql107.prv.f1.k8.com.br;dbname=plataformatalents', 'plataformatalen', 'talents2018', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
